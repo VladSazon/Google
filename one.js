5101a756b507bb856115f35f9bbe82ec93cba0b6
Vlad <vladsazon27@gmail.com>
2016-08-21 10:54:13 +0300

    changes

--- one.js
+++ one.js
@@ -1,3 +1,22 @@
 alert('Helloworld!');
 prompt("Are u okay ?")
 confirm("did u get it ?")
+function billionaires(name, wealth) {
+this.name = name;
+this.wealth = wealth;
+}
+function therichest(name) {
+	this.name = name;
+	this.source = IT;
+}
+function goldenyouth(name) {
+
+	this.name = name;
+	this.saying = "oh yeahh"
+}
+therichest.prototype = new billionaires();
+goldenyouth.prototype = new therichest();
+
+var new = new goldenyouth("Vlad junior");
+console.log(new.saying);
+console.log(new.source);
